2f4b6d824a4a4afba1a9e92a81ea462ee65033c4
Martin Xu <martin.xus@gmail.com>
2018-01-18 11:12:47 +0800

    script for deployment

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+require 'mina/bundler'
+require 'mina/rails'
+require 'mina/git'
+require 'mina/rbenv'
+
+set :domain,      'qlear-web'
+set :user,        'deploy'
+set :deploy_to,   '/data/www/qlear-blog'
+set :repository,  'git@github.com:rdd-giga/qlear-blog.git'
+set :branch,      'master'
+
+task :environment do
+  invoke :'rbenv:load'
+end
+
+desc "Deploying blog ..."
+task :deploy => :environment do
+  deploy do
+    invoke :'git:clone'
+    invoke :'bundle:install'
+    queue "#{bundle_prefix} jekyll build"
+  end
+end
