ed5f026d23b3166ee5052582592b2d4042e9f722
Martin Xu <martin.xus@gmail.com>
2018-01-18 11:47:46 +0800

    update script

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,13 @@ set :repository,  'git@github.com:rdd-giga/qlear-blog.git'
 set :branch,      'master'
 
 task :environment do
+  queue %[export JEKYLL_ENV=production]
   invoke :'rbenv:load'
+end
+
+task :setup => :environment do
+  queue! %[mkdir -p "#{deploy_to}/public"]
+  queue! %[chmod g+rx-w,u+rwx "#{deploy_to}/public"]
 end
 
 desc "Deploying blog ..."
@@ -18,6 +24,8 @@ task :deploy => :environment do
   deploy do
     invoke :'git:clone'
     invoke :'bundle:install'
-    queue "#{bundle_prefix} jekyll build"
+    invoke :'deploy:cleanup'
+
+    queue %[bundle exec jekyll build -d #{deploy_to}/public]
   end
 end
